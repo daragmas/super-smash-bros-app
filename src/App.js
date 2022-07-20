0f85352f31486dd085e567b20d8c32e974be838f
daragmas <daragmas@gmail.com>
2022-07-20 11:47:03 -0400

    added fighter images and styling

--- src/App.js
+++ src/App.js
@@ -7,6 +7,15 @@ function App() {
     <div className="App">
       <h1>Fighters</h1>
       <div className='fighters-grid'>
+        {
+          fighters.map((element,index)=>{
+            return(
+              <div>
+                <img key={index} src={`https://www.smashbros.com/assets_v2/img/fighter/thumb_a/${element.toLowerCase()}.png`}/>
+              </div>
+            )
+          })
+        }
 
       </div>
     </div>
